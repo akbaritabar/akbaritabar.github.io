69f08d5db3b371ff03550759d473c2c07fbe0148
akbaritabar <akbaritabar@gmail.com>
2022-04-16 16:32:41 +0200

    A list of blog posts added

--- docs/convert_script.sh
+++ docs/convert_script.sh
@@ -25,6 +25,19 @@ You can see a CV of me [here](./CV_MD/Aliakbar_Akbaritabar_CV.html)
 ## Computational Social Science Events Worldwide
 You can see the calendar I am publicly maintaining [here](./CV_MD/CSS_events_calendar.html)
 
+## Blog posts from older website
+My older website (available here: ) had some blog posts on data science and analysis related topics. Some are available here:
+
+<ul>
+<li><a href="./CV_MD/out_of_memory_ETL.html"><strong>Parallelized and out of memory data analysis using Dask in Python and DuckDB and DBeaver in SQL. Using example of publicly accessible ORCID 2019 XML files</strong></a></li>
+<li><a href="./CV_MD/How_to_use_a_Wikidata_dump.html"><strong>How to use a Wikidata full json dump, requirements, steps and script</strong></a></li>
+<li><a href="./CV_MD/herss_world_map_flexdashboard.html"><strong>Key Actors in Higher Education Research and Science Studies (HERSS), a Flexdashobrd developed in R</strong></a></li>
+<li><a href="./CV_MD/replicable_network_vis.html"><strong>Get publication-ready, fully replicable network visualizations bringing Visone layouts to R</strong></a></li>
+<li><a href="./CV_MD/gScholarShinyApp.html"><strong>Google Scholar Shiny App</strong></a>: A brief application to work with google scholar and build and show coauthorship networks <a href="https://www.youtube.com/watch?v=NZ5WdBnZ-CE">(see video intro here)</a></li>
+<li><a href="./CV_MD/git_github_for_academic_writing.html"><strong>Git &amp; Github (or Gitlab) for academic writing; UPDATE: advanced topics video added</strong></a>: How do I use git and github to version control my academic writing projects as a social (data) scientist!</li>
+</ul>
+
+
 ## Presentations on reverse chronological order
 
 Click on links to open each year's presentations.
